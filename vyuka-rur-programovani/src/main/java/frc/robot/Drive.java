e94b7e0bc27f56badf6c1ca1eaaf7cb2d2137f31
dasevo <dasek.vojtech@gmail.com>
2021-10-17 19:29:10 +0200

    Vytvoreni funkce pro rizeni robota

--- vyuka-rur-programovani/src/main/java/frc/robot/Drive.java
+++ vyuka-rur-programovani/src/main/java/frc/robot/Drive.java
@@ -8,4 +8,20 @@ public class Drive {
         robotMap = new RobotMap(); //prirazeni hodnoty k promenne typu RobotMap, abychom meli pristup ke vsem funkcim a promennym uvnicr teto funcke
     }
 
+    /**
+     * Funkce pro rizeni robota
+     */
+    public void drive() {
+        robotMap.drive.arcadeDrive(robotMap.getLeftY(), robotMap.getRightX()); 
+        //vyuziti funkce drive (patrici ke tride DifferentialDrive), do ktere dosadime funkce z objektu robotMap
+        //nyni by se robot mel pohybovat pomoci Y osy leveho joysticku (dopredu/dozadu) a X osy praveho joysticku (otaceni)
+    }
+
+    /**
+     * Funkce pro shromazdovani periodickych funkci v ramci tridy Drive
+     */
+    public void periodic() { //v tuto chvili ma tato funkce maly vyznam, jakmile se v teto tride objevi vice funkci, ktere se maji odehravat periodicky, nabyde tato funkce vetsiho vyznamu
+        drive();
+    }
+
 }
